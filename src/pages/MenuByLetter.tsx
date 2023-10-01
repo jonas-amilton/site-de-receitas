9e6bac04d23fb5bf9645f83b285b6eee0bf29edf
jonas-amilton <jonas96@edu.unisinos.br>
2023-10-01 12:49:50 -0300

    [refactor] pagina

--- src/pages/MenuByLetter.tsx
+++ src/pages/MenuByLetter.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Grid } from "@mui/material";
-import {TitlePage} from "../components/index";
+import { AlphabetButtons, TitlePage } from "../components/index";
 
 export const MenuByLetter: React.FC = () => {
   return (
     <Grid container spacing={2}>
-      <TitlePage
-        title="Receitas por Letra"
-      />
+      <TitlePage title="Receitas por Letra" />
+      <Grid item xs={12}>
+        <AlphabetButtons />
+      </Grid>
     </Grid>
   );
 };
