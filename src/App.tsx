472e10ef2847a69d56dcfcf1d5568e0826d60959
jonas-amilton <jonas96@edu.unisinos.br>
2023-10-01 09:27:40 -0300

    [refactor]

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import { DefaultLayout } from "./config/layout/DefaultLayout";
 import { Routers } from "./Routers/Routers";
+import { CssBaseline } from "@mui/material";
+
 
 function App() {
   return (
-    <BrowserRouter>
-    <DefaultLayout>
-      <Routers />
-    </DefaultLayout>
-  </BrowserRouter>
+
+        <BrowserRouter>
+          <CssBaseline />
+          <DefaultLayout>
+            <Routers />
+          </DefaultLayout>
+        </BrowserRouter>
+
   );
 }
 
