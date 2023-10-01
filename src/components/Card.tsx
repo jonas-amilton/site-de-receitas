9af610c0f30d0f30d5b849187f01aafd4ae1ad82
jonas-amilton <jonas96@edu.unisinos.br>
2023-10-01 09:28:31 -0300

    [feat] consumo de 10 receitas da api

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,33 +1,35 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
 
-export default function MultiActionAreaCard() {
+export default function MultiActionAreaCard(props: {
+  title: string;
+  description: string;
+  image: string;
+  urlYoutube: string;
+ 
+}) {
+  const { title, description, image, urlYoutube } = props;
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
+        <CardMedia component="img" height="140" image={image} alt={title} />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            {description}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          Share
+        <Button size="small" color="primary" href={urlYoutube} target="_blank">
+          Youtube
         </Button>
       </CardActions>
     </Card>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,58 @@
 import React, { useEffect, useState } from "react";
-import { Grid, Typography, Card, CardContent } from "@mui/material";
-import TextField from "@mui/material/TextField";
+import { Grid } from "@mui/material";
 import TitlePage from "../components/TitlePage";
+import axios from "axios";
+import MultiActionAreaCard from "../components/Card";
+
+// Função para limitar a descrição para um número específico de palavras
+const limitDescription = (description: string, numWords: number) => {
+  const words = description.split(" ");
+  if (words.length <= numWords) {
+    return description;
+  }
+  const limitWords = words.slice(0, numWords);
+  return `${limitWords.join(" ")}...`;
+};
 
 export const Home: React.FC = () => {
+  const [recipes, setRecipes] = useState<any[]>([]);
 
+  const getRandomRecipes = async () => {
+    try {
+      // IDs das receitas
+      const recipeIds = [
+        52938, 52892, 52833, 52796, 52772, 52765, 52940, 52939, 52942, 52944,
+      ];
+      const recipesData = await Promise.all(
+        recipeIds.map(async (id) => {
+          const response = await axios.get(
+            `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+          );
+          return response.data.meals ? response.data.meals[0] : null;
+        })
+      );
+      setRecipes(recipesData.filter((recipe) => recipe !== null));
+    } catch (error) {
+      console.error("Erro ao buscar receitas:", error);
+    }
+  };
+
+  useEffect(() => {
+    getRandomRecipes();
+  }, []);
 
   return (
     <Grid container spacing={2}>
-    <TitlePage title="Receitas Aleatórias" />
+      <TitlePage title="Receitas Aleatórias" />
+      {recipes.map((recipe) => (
+        <MultiActionAreaCard
+          key={recipe.idMeal}
+          title={recipe.strMeal}
+          description={limitDescription(recipe.strInstructions, 20)} // Limita para 20 palavras
+          image={recipe.strMealThumb}
+          urlYoutube={recipe.strYoutube}
+        />
+      ))}
     </Grid>
   );
 };
