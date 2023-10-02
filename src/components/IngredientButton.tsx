f3aa26676ae6ade67314767077f56413ad46c64d
jonas-amilton <jonas96@edu.unisinos.br>
2023-10-02 09:31:09 -0300

    [refactor] componente - pagina ingredientes

--- src/components/IngredientButton.tsx
+++ src/components/IngredientButton.tsx
@@ -1,20 +1,49 @@
-import React from "react";
-import { Button, Stack } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Button, Grid } from "@mui/material";
+import axios from "axios";
 
 interface IngredientButtonProps {
-  ingredient: string;
+  ingredient?: string;
 }
 
-export const IngredientButton: React.FC<IngredientButtonProps> = ({ ingredient }) => {
+export const IngredientButton: React.FC<IngredientButtonProps> = () => {
+  const [ingredients, setIngredients] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      try {
+        const response = await axios.get(
+          "https://www.themealdb.com/api/json/v1/1/list.php?i=list"
+        );
+
+        if (response.data && response.data.meals) {
+          const ingredientNames = response.data.meals.map(
+            (meal: any) => meal.strIngredient
+          );
+          setIngredients(ingredientNames);
+          console.log(ingredients);
+          
+        }
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    };
+
+    fetchIngredients();
+  }, []);
+
   return (
-    <Stack direction="row" spacing={2}>
-      <Button
-        style={{ margin: "1em", color: "#000", backgroundColor: "#fff" }}
-        sx={{ width: "36em" }}
-        variant="contained"
-      >
-        {ingredient}
-      </Button>
-    </Stack>
+    <Grid item xs={12} style={{ display: "contents" }}>
+      {ingredients.map((ingredientName) => (
+        <Button
+          key={ingredientName}
+          style={{ margin: "1em", color: "#000", backgroundColor: "#fff" }}
+          sx={{ width: "36em" }}
+          variant="contained"
+        >
+          {ingredientName}
+        </Button>
+      ))}
+    </Grid>
   );
 };
--- src/pages/MenuIngredients.tsx
+++ src/pages/MenuIngredients.tsx
@@ -4,35 +4,13 @@ import { TitlePage, IngredientButton } from "../components/index";
 import axios from "axios";
 
 export const MenuIngredients: React.FC = () => {
-  const [ingredients, setIngredients] = useState<string[]>([]);
-
-  useEffect(() => {
-    const fetchIngredients = async () => {
-      try {
-        const response = await axios.get(
-          "https://www.themealdb.com/api/json/v1/1/list.php?i=list"
-        );
-
-        if (response.data && response.data.meals) {
-          const ingredientNames = response.data.meals.map(
-            (meal: any) => meal.strIngredient
-          );
-          setIngredients(ingredientNames);
-        }
-      } catch (error) {
-        console.error("Erro ao buscar dados:", error);
-      }
-    };
-
-    fetchIngredients();
-  }, []);
-
+ 
   return (
     <Grid container spacing={2}>
       <TitlePage title="Ingredientes" />
-      {ingredients.map((ingredient) => (
-        <IngredientButton key={ingredient} ingredient={ingredient} />
-      ))}
+  
+        <IngredientButton />
+
     </Grid>
   );
 };
