99e8e66cb4eb9165b8730ea7983a28a30d3dc82a
jonas-amilton <jonas96@edu.unisinos.br>
2023-10-03 04:43:35 -0300

    [new] interface

---
+++ src/Interfaces/IRecipe.ts
@@ -0,0 +1,11 @@
+export interface IRecipe {
+    idMeal: string;
+    strMeal: string;
+    strInstructions: string;
+    strMealThumb: string;
+    strYoutube: string;
+    strCategory: string;
+    strArea: string;
+    strTags: string;
+    recipe: string;
+  }
