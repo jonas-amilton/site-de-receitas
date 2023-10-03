d6172a1a613a8ee7ce38a5e68f389a6882f3983e
jonas-amilton <jonas96@edu.unisinos.br>
2023-10-03 05:58:22 -0300

    [delete] redux - estado global

--- src/store/modules/rootReducer.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { combineReducers } from '@reduxjs/toolkit';
-import randomMealReducer from './randomMealSlice';
-
-export default combineReducers({
-  randomMeal: randomMealReducer,
-});
